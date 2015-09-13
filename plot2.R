57ce4a88673f2c1305632c327a498eada1f7075b
FSobieczky <Florian.Sobieczky@du.edu>
2015-09-13 19:24:04 +0200

    plot2; Update plot1.R

--- plot2.R
+++ plot2.R
@@ -1,4 +1,4 @@
-# plot1.R  (Florian Sobieczky - Coursera)
+# plot2.R  (Florian Sobieczky - Coursera)
 
 T<-read.csv("household_power_consumption.txt", sep=";");
 TT<-as.matrix(T);   # more convenient to deal with matrix
@@ -8,12 +8,16 @@ W2<-TT[,3][TT[,1]=="2/2/2007"];  # Extract second day
 W<-append(W1, W2, after = length(W1)) # put them together in one vector
                                  # Note: W<-TT[,3][TT[,1]=="2/1/2007" || TT[,1]=="2/2/2007"]
   				 #       didn't work for some reason ....
-V<-na.omit(as.numeric(W)); 	 # Make the type numeric, through out `NA's'.
-png("plot1.png");		 # Generate the png-file.
-hist(V,  main="Global Active Power", col="red", xlab="Global Active Power (kilowatts)",ylab="Frequency");
-dev.off();
 
+png("plot2.png");		 # Generate the png-file.
 
+par(mar = c(3,4,2,2));           # choice of 4 for more space on left side
+plot(W, ylab="Global Active Power (kilowatts)", type="l", xaxt='n');
+axis(1, at = c(1,length(W)/2,length(W)), labels=c("Thu","Fri","Sat"));
+    # Note: xaxt='n' first switches standard xlabels off, axis(...) puts Thu,Fri,Sat there
+dev.off(); 
+
+	
 
 
 
