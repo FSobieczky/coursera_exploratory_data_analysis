b34d7341521386803a9737cc95317b51d30d3e22
FSobieczky <Florian.Sobieczky@du.edu>
2015-09-13 21:13:50 +0200

    Update plot1.R and plot1.png

--- plot1.R
+++ plot1.R
@@ -2,8 +2,10 @@
 
 T<-read.csv("household_power_consumption.txt", sep=";");
 TT<-as.matrix(T);   # more convenient to deal with matrix
+lx<-length(TT[1,]); # will appear more often than once,
+ly<-length(TT[,1]); # adds to clarity to make these defs.
 
-W1<-TT[,3][TT[,1]=="2/1/2007"];  # Extract first day
+W1<-TT[,3][TT[,1]=="1/2/2007"];  # Extract first day
 W2<-TT[,3][TT[,1]=="2/2/2007"];  # Extract second day
 W<-append(W1, W2, after = length(W1)) # put them together in one vector
                                  # Note: W<-TT[,3][TT[,1]=="2/1/2007" || TT[,1]=="2/2/2007"]
